8b96a8b9b25b1af4b0e3bef28305d6b250baf11d
liangzai <1270972937@qq.com>
2017-06-18 13:52:50 +0800

    test python project and python package

--- com/test/test.py
+++ com/test/test.py
@@ -3,9 +3,13 @@
 @author: liangzai
 '''
 
+from com.util import mydef
+
 if __name__ == '__main__':
     for i in range(5):
         print(i)
     print("hello world.")
     for x in range(1, 5):
         print(x)
+
+    print("random: %s" % (mydef.get_random(1, 10)))
